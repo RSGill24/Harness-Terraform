5795750d85f87ca87025ef5853435107763d492a
RSGill24 <rajindergill2024@gmail.com>
2024-05-11 02:08:37 +0530

    Update variables.tf

--- gcp/compute/basic_vm/variables.tf
+++ gcp/compute/basic_vm/variables.tf
@@ -22,3 +22,28 @@ variable "machine_type" {
     description = "Gcp VM Machine Type"
     type        = string
 }
+
+variable "vpc_name" {
+    description = "Name of VPC network"
+    type        = string
+}
+
+variable "subnets_name" {
+    description = "Name of Subnets"
+    type        = string
+}
+
+variable "ip_cidr_range" {
+    description = "IP CIDR Range for subnets"
+    type        = string
+}
+
+variable "vm_name" {
+    description = "Name of Virtual Machine"
+    type        = string
+}
+
+variable "vm_disk_image" {
+    description = "Boot disk image type for VM"
+    type        = string
+}
