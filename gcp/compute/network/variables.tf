a4d50112c394e6c0b6196a04b71c5640f430e974
RSGill24 <rajindergill2024@gmail.com>
2024-05-11 17:48:56 +0530

    Update variables.tf

--- gcp/compute/network/variables.tf
+++ gcp/compute/network/variables.tf
@@ -2,3 +2,18 @@ variable "vpc_name" {
     description = "Name of VPC network"
     type        = string
 }
+
+variable "gcp_terraform_sa_creds" {
+    description = "GCP SA Credntial file json format"
+    type        = map(string)
+}
+
+variable "project_id" {
+    description = "GCP Project Id"
+    type        = string
+}
+
+variable "region" {
+    description = "Gcp provider/VM Region"
+    type        = string
+}
