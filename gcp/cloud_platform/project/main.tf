26368e64cd15ff80c8d6982f748dbe2e74772e76
RSGill24 <rajindergill2024@gmail.com>
2024-05-11 17:59:57 +0530

    Create main.tf

---
+++ gcp/cloud_platform/project/main.tf
@@ -0,0 +1,10 @@
+resource "google_project" "project_in_a_folder" {
+    name        = var.project_name
+    project_id  = var.project_id
+    folder_id   = google_folder.department.name
+}
+
+resource "google_folder" "department" {
+    display_name = var.folder_display_name
+    parent       = var.parent_dir
+}
