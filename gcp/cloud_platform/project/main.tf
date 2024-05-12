cb3ae3dd28f6a03c2258aa0569ea5c7180d9e4c8
RSGill24 <rajindergill2024@gmail.com>
2024-05-13 01:22:10 +0530

    Update main.tf

--- gcp/cloud_platform/project/main.tf
+++ gcp/cloud_platform/project/main.tf
@@ -10,6 +10,6 @@ resource "google_project" "project_in_a_folder" {
 #}
 
 resource "google_project_service" "compute" {
-    project = google_project.my_project.project_id
+    project = google_project.project_in_a_folder.project_id
     service = "compute.googleapis.com"
 }
