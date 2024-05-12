794cc628037596a3d890d7ea3c8ba2bce1e46c78
RSGill24 <rajindergill2024@gmail.com>
2024-05-12 20:35:00 +0530

    Create main.tf

---
+++ gcp/cloud_storage/bucket/main.tf
@@ -0,0 +1,18 @@
+resource "google_storage_bucket" "sample_bucket" {
+  name          = var.name
+  location      = var.location
+  force_destroy = var.destroy
+
+  public_access_prevention = var.public_access_prevention
+
+  lifecycle_rule {
+    condition {
+      age = var.age
+    }
+    action {
+      type = var.action_type
+    }
+  }
+
+  uniform_bucket_level_access = var.uniform_bucket_level_access
+}
