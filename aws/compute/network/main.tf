73a62298a1c48096b77e8a710abfebcc922d8b59
RSGill24 <rajindergill2024@gmail.com>
2024-05-12 19:53:32 +0530

    Create main.tf

---
+++ aws/compute/network/main.tf
@@ -0,0 +1,26 @@
+data "aws_region" "current" {}
+
+resource "aws_vpc_ipam" "test" {
+  operating_regions {
+    region_name = data.aws_region.current.name
+  }
+}
+
+resource "aws_vpc_ipam_pool" "test" {
+  address_family = var.address_family
+  ipam_scope_id  = aws_vpc_ipam.test.private_default_scope_id
+  locale         = data.aws_region.current.name
+}
+
+resource "aws_vpc_ipam_pool_cidr" "test" {
+  ipam_pool_id = aws_vpc_ipam_pool.test.id
+  cidr         = var.cidr
+}
+
+resource "aws_vpc" "test" {
+  ipv4_ipam_pool_id   = aws_vpc_ipam_pool.test.id
+  ipv4_netmask_length = var.ipv4_netmask_length
+  depends_on = [
+    aws_vpc_ipam_pool_cidr.test
+  ]
+}
