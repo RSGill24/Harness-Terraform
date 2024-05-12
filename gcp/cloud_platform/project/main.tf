ae72db522d291f94c0bb6564cc53fdfc2a5337b1
RSGill24 <rajindergill2024@gmail.com>
2024-05-13 00:51:10 +0530

    Update main.tf

--- gcp/cloud_platform/project/main.tf
+++ gcp/cloud_platform/project/main.tf
@@ -1,10 +1,10 @@
 resource "google_project" "project_in_a_folder" {
     name        = var.project_name
     project_id  = var.project_id
-    folder_id   = google_folder.department.name
+#    folder_id   = google_folder.department.name
 }
 
-resource "google_folder" "department" {
-    display_name = var.folder_display_name
-    parent       = var.parent_dir
-}
+#resource "google_folder" "department" {
+#    display_name = var.folder_display_name
+#    parent       = var.parent_dir
+#}
