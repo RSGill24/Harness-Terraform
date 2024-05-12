2e45f1f05feb43918ec197088f7fc348816dd7ba
RSGill24 <rajindergill2024@gmail.com>
2024-05-12 20:40:02 +0530

    Create variables.tf

---
+++ gcp/cloud_storage/bucket/variables.tf
@@ -0,0 +1,49 @@
+variable "gcp_terraform_sa_creds" {
+    description = "GCP SA Credntial file json format"
+    type        = map(string)
+}
+
+variable "project_id" {
+    description = "GCP Project Id"
+    type        = string
+}
+
+variable "region" {
+    description = "Gcp provider Region"
+    type        = string
+}
+
+variable "name" {
+    description = "Name of cloud storage"
+    type        = string
+}
+
+variable "location" {
+    description = "location of cloud storage"
+    type        = string
+}
+
+variable "destroy" {
+    description = "Should force destroy cloud storage"
+    type        = bool
+}
+
+variable "public_access_prevention" {
+    description = "Should prevent public access for cloud storage"
+    type        = bool
+}
+
+variable "uniform_bucket_level_access" {
+    description = "Should provide uniform bucket level access"
+    type        = bool
+}
+
+variable "age" {
+    description = "lifecycle age of cloud storage"
+    type        = number
+}
+
+variable "action_type" {
+    description = "lifecycle end action type"
+    type        = string
+}
