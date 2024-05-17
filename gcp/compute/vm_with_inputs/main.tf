61cb842cc74632d3ba2f085e6ea83113595b22c5
RSGill24 <rajindergill2024@gmail.com>
2024-05-18 01:34:12 +0530

    Create main.tf

---
+++ gcp/compute/vm_with_inputs/main.tf
@@ -0,0 +1,25 @@
+resource "google_compute_instance" "custom_subnet" {
+  name         = var.vm_name
+  zone         = var.zone
+  machine_type = var.machine_type
+  network_interface {
+    network    = google_compute_network.custom.id
+    subnetwork = google_compute_subnetwork.custom.id
+  }
+  boot_disk {
+    initialize_params {
+      image = var.vm_disk_image
+    }
+  }
+  metadata = {
+    block-project-ssh-keys = var.block_project_ssh_keys
+  }
+#  network_interface {
+#    network = "default"
+#
+#    access_config {
+#      nat_ip = var.nat_ip
+#      network_tier = var.network_tier
+#    }
+#  }
+}
