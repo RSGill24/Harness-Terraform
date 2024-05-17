70ae56f3363af1bc46a6739d97de14ec8b1ec928
RSGill24 <rajindergill2024@gmail.com>
2024-05-18 01:35:44 +0530

    Create variables.tf

---
+++ gcp/compute/vm_with_inputs/variables.tf
@@ -0,0 +1,39 @@
+variable "gcp_terraform_sa_creds" {
+    description = "GCP SA Credntial file json format"
+    type        = map(string)
+}
+
+variable "project_id" {
+    description = "GCP Project Id"
+    type        = string
+}
+
+variable "region" {
+    description = "Gcp provider/VM Region"
+    type        = string
+}
+
+variable "zone" {
+    description = "Gcp VM Zone"
+    type        = string
+}
+
+variable "machine_type" {
+    description = "Gcp VM Machine Type"
+    type        = string
+}
+
+variable "vm_name" {
+    description = "Name of Virtual Machine"
+    type        = string
+}
+
+variable "vm_disk_image" {
+    description = "Boot disk image type for VM"
+    type        = string
+}
+
+variable "block_project_ssh_keys" {
+    description = "block project ssh keys"
+    type        = bool
+}
