15eb87d89b3d138de6495bd13ac07a0cce131bcf
RSGill24 <rajindergill2024@gmail.com>
2024-05-18 01:22:11 +0530

    Update output.tf

--- gcp/compute/sub_network/output.tf
+++ gcp/compute/sub_network/output.tf
@@ -1,1 +1,9 @@
+output "network_id" {
+    description = "VPC network Id"
+    value       = google_compute_instance.custom_subnet.instance_id
+}
 
+output "sub_network_id" {
+    description = "Subnetwork Id"
+    value       = google_compute_instance.custom_subnet.instance_id
+}
