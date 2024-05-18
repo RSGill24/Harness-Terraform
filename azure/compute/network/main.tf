c601f0681af0bd97cd50a386341094f8a351214d
RSGill24 <rajindergill2024@gmail.com>
2024-05-18 21:17:57 +0530

    Create main.tf

---
+++ azure/compute/network/main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.network_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = var.address_space
+  dns_servers         = var.dns_servers
+
+  subnet {
+    name           = var.subnet_name
+    address_prefix = var.address_prefix
+  }
+
+  tags = {
+    environment = var.environment_tag
+  }
+}
