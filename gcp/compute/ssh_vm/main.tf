93e526f91c58499c6ad55bdb358d942ed269cd54
RSGill24 <rajindergill2024@gmail.com>
2024-07-02 19:35:37 +0530

    Update main.tf

--- gcp/compute/ssh_vm/main.tf
+++ gcp/compute/ssh_vm/main.tf
@@ -1,39 +1,53 @@
-resource "google_compute_network" "custom" {
+resource "google_compute_network" "vpc_network" {
   name                    = var.vpc_name
   auto_create_subnetworks = true
 }
 # [END vpc_compute_basic_vm_custom_vpc_network]
 
-# [START vpc_compute_basic_vm_custom_vpc_subnet]
-resource "google_compute_subnetwork" "custom" {
-  name          = var.subnets_name
-  ip_cidr_range = var.ip_cidr_range
-  region        = var.region
-  network       = google_compute_network.custom.id
+
+resource "google_compute_firewall" "default" {
+  name    = "default-allow-ssh"
+  network = google_compute_network.vpc_network.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
 }
-
-# [END vpc_compute_basic_vm_custom_vpc_subnet]
-
-# [START compute_instances_create_with_subnet]
 
 # Create a VM in a custom VPC network and subnet
 
-resource "google_compute_instance" "custom_subnet" {
+resource "google_compute_instance" "ssh_vm" {
   name         = var.vm_name
   zone         = var.zone
   machine_type = var.machine_type
   network_interface {
-    network    = google_compute_network.custom.id
-    subnetwork = google_compute_subnetwork.custom.id
+    network    = google_compute_network.vpc_network.id
 
-    access_config {}
+    access_config {
+      # Include this to give the VM an external IP address
+    }
   }
+
+  metadata = {
+    sshKeys = "${var.ssh_user}:${var.ssh_key}"
+  }
+
+  tags = ["ssh"]
+
   boot_disk {
     initialize_params {
       image = var.vm_disk_image
     }
   }
-  metadata = {
-    sshKeys = "${var.ssh_user}:${var.ssh_key}"
+
+  service_account {
+    email  = var.service_account
+    scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ]
   }
 }
