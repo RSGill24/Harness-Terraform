41e794b140c06f8f9981a436073368cc13f840c5
RSGill24 <rajindergill2024@gmail.com>
2024-07-21 20:53:57 +0530

    Update main.tf

--- gcp/compute/workspace_vm/main.tf
+++ gcp/compute/workspace_vm/main.tf
@@ -1,80 +1,53 @@
-/**
- * Copyright 2023 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-# [START compute_basic_vm_parent_tag]
-# [START compute_instances_create]
-
-# Create a VM instance from a public image
-# in the `default` VPC network and subnet
-
-# [START vpc_compute_basic_vm_custom_vpc_network]
-resource "google_compute_network" "custom" {
+resource "google_compute_network" "vpc_network" {
   name                    = var.vpc_name
   auto_create_subnetworks = true
 }
 # [END vpc_compute_basic_vm_custom_vpc_network]
 
-# [START vpc_compute_basic_vm_custom_vpc_subnet]
-resource "google_compute_subnetwork" "custom" {
-  name          = var.subnets_name
-  ip_cidr_range = var.ip_cidr_range
-  region        = var.region
-  network       = google_compute_network.custom.id
-}
 
-resource "google_compute_firewall" "deny_rdp" {
-  name         = var.firewall_name
-  network      = google_compute_network.custom.self_link
-  source_tags = ["web"]
-  target_tags  = ["bastion"]
-  deny {
-      protocol = var.protocol
-      ports    = var.ports
+resource "google_compute_firewall" "default" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
   }
-}
-# [END vpc_compute_basic_vm_custom_vpc_subnet]
 
-# [START compute_instances_create_with_subnet]
+  source_ranges = ["0.0.0.0/0"]
+}
 
 # Create a VM in a custom VPC network and subnet
 
-resource "google_compute_instance" "custom_subnet" {
+resource "google_compute_instance" "ssh_vm" {
   name         = var.vm_name
   zone         = var.zone
   machine_type = var.machine_type
   network_interface {
-    network    = google_compute_network.custom.id
-    subnetwork = google_compute_subnetwork.custom.id
+    network    = google_compute_network.vpc_network.id
+
+    access_config {
+      # Include this to give the VM an external IP address
+    }
   }
+
+  metadata = {
+    sshKeys = "${var.ssh_user}:${var.ssh_key}"
+  }
+
+  tags = ["ssh"]
+
   boot_disk {
     initialize_params {
       image = var.vm_disk_image
     }
   }
-  metadata = {
-    block-project-ssh-keys = var.block_project_ssh_keys
+
+  service_account {
+    email  = var.service_account
+    scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ]
   }
-#  network_interface {
-#    network = "default"
-#
-#    access_config {
-#      nat_ip = var.nat_ip
-#      network_tier = var.network_tier
-#    }
-#  }
 }
-# [END compute_instances_create_with_subnet]
-# [END compute_basic_vm_parent_tag]
