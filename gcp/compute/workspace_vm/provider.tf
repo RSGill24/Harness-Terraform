aabc2a4307045231aa22846df331bf7c38bf68e6
RSGill24 <rajindergill2024@gmail.com>
2024-07-21 20:54:46 +0530

    Update provider.tf

--- gcp/compute/workspace_vm/provider.tf
+++ gcp/compute/workspace_vm/provider.tf
@@ -11,5 +11,5 @@ terraform {
 provider "google" {
     project     = var.project_id
     region      = var.region
-    credentials = jsonencode(var.gcp_terraform_sa_creds)
+    credentials = file(var.gcp_terraform_sa_creds)
 }
