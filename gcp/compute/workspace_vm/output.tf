7b1fd0971ec3ed629f58eb7247da61bdc6cfff0a
RSGill24 <rajindergill2024@gmail.com>
2024-07-21 20:54:14 +0530

    Update output.tf

--- gcp/compute/workspace_vm/output.tf
+++ gcp/compute/workspace_vm/output.tf
@@ -1,4 +1,9 @@
 output "instance_id" {
     description = "Instance Id"
-    value       = google_compute_instance.custom_subnet.instance_id
+    value       = google_compute_instance.ssh_vm.instance_id
+}
+
+output "external_ip" {
+  description = "The external IP address of the VM instance"
+  value       = google_compute_instance.ssh_vm.network_interface.0.access_config.0.nat_ip
 }
